5de0efb565a3147cca242e65556a3bd05400608d
Lalithavanukuri <95462097+Lalithavanukuri@users.noreply.github.com>
2025-09-16 03:43:40 +0530

    Add files via upload

---
+++ src/test/java/com/springspartans/shopkart/repository/CustomerRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.springspartans.shopkart.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerRepositoryTest {
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void countBySignup_dateBetween() {
+    }
+
+    @Test
+    void countByLast_login_dateBetween() {
+    }
+}
